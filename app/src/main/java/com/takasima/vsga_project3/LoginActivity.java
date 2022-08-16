bdcdc77a39e260b1652b324d4e767e4640dd9894
Bonifasius Tarigan <bonifasius.tambak@gmail.com>
2022-08-16 21:45:20 +0700

    registrasi screen

--- app/src/main/java/com/takasima/vsga_project3/LoginActivity.java
+++ app/src/main/java/com/takasima/vsga_project3/LoginActivity.java
@@ -2,13 +2,33 @@ package com.takasima.vsga_project3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText etUsername, etPassword;
+    Button btnLogin, btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnRegister = findViewById(R.id.btn_register) ;
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
     }
 }
--- app/src/main/java/com/takasima/vsga_project3/RegisterActivity.java
+++ app/src/main/java/com/takasima/vsga_project3/RegisterActivity.java
@@ -3,12 +3,80 @@ package com.takasima.vsga_project3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText inputUsername, inputPassword, inputEmail, inputNama, inputAsal, inputAlamat;
+    Button btnSimpan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        this.setTitle("Register");
+        inputUsername = findViewById(R.id.et_usernameR);
+        inputPassword = findViewById(R.id.et_passwordR);
+        inputEmail = findViewById(R.id.et_emailR);
+        inputNama = findViewById(R.id.et_namaR);
+        inputAsal = findViewById(R.id.et_asalSekolahR);
+        inputAlamat = findViewById(R.id.et_alamatR);
+        btnSimpan = findViewById(R.id.btn_simpan);
+
+        btnSimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isValid()){
+                    simpanFileData();
+                }else{
+                    Toast.makeText(RegisterActivity.this, "Mohon Lenkapi Seluruh Data", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public boolean isValid(){
+        if (inputUsername.getText().toString().equals("")||
+                inputPassword.getText().toString().equals("")||
+                inputEmail.getText().toString().equals("")||
+                inputNama.getText().toString().equals("")||
+                inputAsal.getText().toString().equals("")||
+                inputAlamat.getText().toString().equals("")){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    void simpanFileData(){
+        String isiFile = inputUsername.getText().toString() + ";"
+                + inputEmail.getText().toString() + ";"
+                + inputNama.getText().toString() + ";"
+                + inputAsal.getText().toString() + ";"
+                + inputAlamat.getText().toString();
+
+        File file = new File(getFilesDir(), inputUsername.getText().toString());
+        FileOutputStream outputStream = null;
+
+
+        try {
+            file.createNewFile(); //Buat file baru
+            outputStream = new FileOutputStream(file, false);
+            outputStream.write(isiFile.getBytes());
+            outputStream.flush();
+            outputStream.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        Toast.makeText(this, "Registrasi Berhasil!", Toast.LENGTH_SHORT).show();
+        onBackPressed();
     }
 }
